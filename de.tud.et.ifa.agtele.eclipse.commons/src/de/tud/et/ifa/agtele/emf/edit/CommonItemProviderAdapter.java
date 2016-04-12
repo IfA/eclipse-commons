ea2b54adaa4694e758343fe512cc6cf6c4862803
mfreund <matth.freund@gmail.com>
2016-04-12 17:19:54 +0200

    Implement 'createDragAndDropCommand' in the item provider adapter

--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/CommonItemProviderAdapter.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/CommonItemProviderAdapter.java
@@ -1,6 +1,10 @@
 package de.tud.et.ifa.agtele.emf.edit;
 
+import java.util.Collection;
+
+import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
 
 /**
@@ -15,6 +19,18 @@ public class CommonItemProviderAdapter extends ItemProviderAdapter {
 
 	public CommonItemProviderAdapter(AdapterFactory adapterFactory) {
 		super(adapterFactory);
+	}
+
+	@Override
+	protected Command createDragAndDropCommand(EditingDomain domain, Object owner, float location, int operations,
+			int operation, Collection<?> collection) {
+		
+		// If possible use the special functionality provided by the 'IDragAndDropProvider' interface...
+		if(this instanceof IDragAndDropProvider) {
+			return ((IDragAndDropProvider) this).createCustomDragAndDropCommand(domain, owner, location, operations, operation, collection);
+		} else {
+			return super.createDragAndDropCommand(domain, owner, location, operations, operation, collection);
+		}
 	}
 
 }
