5da7ddb7c669bfc09d60a4d5f3f7bd2b92d7b403
mfreund <matth.freund@gmail.com>
2016-10-27 14:21:34 +0200

    Control the creation of the OpenCode/GenModel handlers via a custom menu

---
+++ de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/contributions/EcoreEditorMenuContribution.java
@@ -0,0 +1,26 @@
+package de.tud.et.ifa.agtele.ui.contributions;
+
+import org.eclipse.jface.action.Separator;
+import org.eclipse.ui.menus.ExtensionContributionFactory;
+import org.eclipse.ui.menus.IContributionRoot;
+import org.eclipse.ui.services.IServiceLocator;
+
+/**
+ * An {@link ExtensionContributionFactory} that uses the
+ * {@link OpenGenModelMenuContribution} and the {@link OpenCodeMenuContribution}
+ * to assemble the menu contribution for an Ecore model editor.
+ *
+ * @author mfreund
+ */
+public class EcoreEditorMenuContribution extends ExtensionContributionFactory {
+
+	@Override
+	public void createContributionItems(IServiceLocator serviceLocator, IContributionRoot additions) {
+
+		additions.addContributionItem(new Separator("begin-agtele-ecore-additions"), null);
+		new OpenGenModelMenuContribution().createContributionItems(serviceLocator, additions);
+		new OpenCodeMenuContribution().createContributionItems(serviceLocator, additions);
+		additions.addContributionItem(new Separator("end-agtele-ecore-additions"), null);
+	}
+
+}
