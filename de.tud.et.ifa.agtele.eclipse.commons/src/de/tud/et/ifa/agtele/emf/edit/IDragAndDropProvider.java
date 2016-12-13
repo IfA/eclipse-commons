df9e17d766a5f6ee96c41b13fee9f1d4281ae99a
mfreund <matth.freund@gmail.com>
2016-12-13 12:00:04 +0100

    Improve behaviour of the 'IDragAndDropProvider'
    Do not only check the property/newChild descriptors to check if values
    are supported but also take into account the type of the reference. This
    is used to classify references by 'enabled/disabled' which is exploited
    by the 'AmbiguousDragAndDropCommandWrapper'.

--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IDragAndDropProvider.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IDragAndDropProvider.java
@@ -1,9 +1,13 @@
 package de.tud.et.ifa.agtele.emf.edit;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,12 +16,14 @@ import org.eclipse.emf.common.command.Command;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.edit.command.AddCommand;
 import org.eclipse.emf.edit.command.CommandParameter;
 import org.eclipse.emf.edit.command.DragAndDropCommand;
 import org.eclipse.emf.edit.domain.EditingDomain;
 import org.eclipse.emf.edit.provider.AdapterFactoryItemDelegator;
 import org.eclipse.emf.edit.provider.IItemPropertyDescriptor;
 import org.eclipse.emf.edit.provider.ItemProviderAdapter;
+
 import de.tud.et.ifa.agtele.emf.AgteleEcoreUtil;
 import de.tud.et.ifa.agtele.emf.edit.commands.AmbiguousDragAndDropCommandWrapper;
 import de.tud.et.ifa.agtele.emf.edit.commands.BasicDragAndDropAddCommand;
@@ -25,171 +31,207 @@ import de.tud.et.ifa.agtele.emf.edit.commands.BasicDragAndDropCompoundCommand;
 import de.tud.et.ifa.agtele.emf.edit.commands.BasicDragAndDropSetCommand;
 import de.tud.et.ifa.agtele.emf.edit.commands.DragAndDropChangeContainingFeatureCommand;
 
-
 /**
- * This provides custom implementations for the '<em>createDragAndDropCommand(...)</em>' function provided by
- * the {@link ItemProviderAdapter}.
- * 
+ * This provides custom implementations for the '<em>createDragAndDropCommand(...)</em>' function provided by the
+ * {@link ItemProviderAdapter}.
+ *
  * @author mfreund
  */
 public interface IDragAndDropProvider {
-	
+
 	/**
-	 * Create a custom {@link DragAndDropCommand}. The given '<em>strategy</em>' can be used to select 
-	 * one of multiple possible commands to execute.
+	 * Create a custom {@link DragAndDropCommand}. The given '<em>strategy</em>' can be used to select one of multiple
+	 * possible commands to execute.
 	 * <p />
-	 * This will usually be called inside an implementation of '<em>createDragAndDropCommand(...)</em>' in a 
-	 * custom ItemProvider instead of just calling '<em>super.createDragAndDropCommand(...)</em>'.
-	 * 
-	 * @param domain The {@link EditingDomain} that will be used to execute the command.
-	 * @param owner The object on that the command will be executed.
-	 * @param location The location (between 0.0 and 1.0) in relation to the '<em>owner</em>' where the command will be executed.
-	 * @param operations The permitted operations.
-	 * @param operation The desired operation.
-	 * @param collection The dragged elements.
-	 * @param strategy The {@link ICommandSelectionStrategy} that shall be applied to select an unambiguous 
-	 * command before execution.
+	 * This will usually be called inside an implementation of '<em>createDragAndDropCommand(...)</em>' in a custom
+	 * ItemProvider instead of just calling '<em>super.createDragAndDropCommand(...)</em>'.
+	 *
+	 * @param domain
+	 *            The {@link EditingDomain} that will be used to execute the command.
+	 * @param owner
+	 *            The object on that the command will be executed.
+	 * @param location
+	 *            The location (between 0.0 and 1.0) in relation to the '<em>owner</em>' where the command will be
+	 *            executed.
+	 * @param operations
+	 *            The permitted operations.
+	 * @param operation
+	 *            The desired operation.
+	 * @param collection
+	 *            The dragged elements.
+	 * @param strategy
+	 *            The {@link ICommandSelectionStrategy} that shall be applied to select an unambiguous command before
+	 *            execution.
 	 * @return The custom drag and drop command.
 	 */
 	@SuppressWarnings("unchecked")
-	public default Command createCustomDragAndDropCommand(EditingDomain domain, Object owner, float location, int operations,
-			int operation, Collection<?> collection, ICommandSelectionStrategy strategy) {
-		
-		DragAndDropCommand dragAndDropCommand = new DragAndDropCommand(domain, owner, location, operations, operation, collection) {
-			
+	public default Command createCustomDragAndDropCommand(EditingDomain domain, Object owner, float location,
+			int operations, int operation, Collection<?> collection, ICommandSelectionStrategy strategy) {
+
+		DragAndDropCommand dragAndDropCommand = new DragAndDropCommand(domain, owner, location, operations, operation,
+				collection) {
+
 			@Override
 			public boolean validate(Object owner, float location, int operations, int operation,
 					Collection<?> collection) {
-				// use the default d'n'd command only for move/copy operations (i.e. if the location is near 0 or near 1)
-				return super.validate(owner, location, operations, operation, collection) && (location < 0.2 || location > 0.8);
+
+				// use the default d'n'd command only for move/copy operations (i.e. if the location is near 0 or near
+				// 1)
+				return super.validate(owner, location, operations, operation, collection)
+						&& (location < 0.2 || location > 0.8);
 			}
 		};
 
-		if(!(owner instanceof EObject) || collection.isEmpty()) {
+		if (!(owner instanceof EObject) || collection.isEmpty()) {
 			return dragAndDropCommand;
 		}
-		
-		// Determine the common super type of all dragged objects (this will be checked against the types of the possible references)
+
+		// Determine the common super type of all dragged objects (this will be checked against the types of the
+		// possible references)
 		// and if all elements are of the exact same type
 		//
 		Set<EClass> eClassSet = new HashSet<>();
-		for (Iterator<?> iterator = collection.iterator(); iterator.hasNext();) {
-			Object object = iterator.next();
-			
-			if(!(object instanceof EObject)) {
+		for (Object object : collection) {
+			if (!(object instanceof EObject)) {
 				return dragAndDropCommand;
 			}
-			
+
 			eClassSet.add(((EObject) object).eClass());
 		}
 
 		EObject parent = (EObject) owner;
 		AdapterFactoryItemDelegator delegator = AgteleEcoreUtil.getAdapterFactoryItemDelegatorFor(parent);
-		ArrayList<EReference> possibleReferences = new ArrayList<>();
-		
-		if(delegator != null) {
-			
+		Map<EReference, Boolean> possibleReferences = new HashMap<>();
+
+		if (delegator != null) {
+
 			// Determine all possible references that could be used to drop the collection
 			//
 			for (EReference ref : parent.eClass().getEAllReferences()) {
-				if((ref.isContainment() || ref.getEOpposite() == null || !ref.getEOpposite().isContainment())
-						&& !ref.isDerived() 
-						&& (collection.size() > 1 ? ref.isMany() : true)) {
-					
-					// If there is an 'IItemPropertyDescriptor' for the reference, we use this to check suitable values...
+				if ((ref.isContainment() || ref.getEOpposite() == null || !ref.getEOpposite().isContainment())
+						&& !ref.isDerived() && (collection.size() > 1 ? ref.isMany() : true) && eClassSet
+								.parallelStream().allMatch(eClass -> ref.getEReferenceType().isSuperTypeOf(eClass))) {
+
+					// If there is an 'IItemPropertyDescriptor' for the reference, we use this to check suitable
+					// values...
 					//
 					IItemPropertyDescriptor propDesc;
 					Collection<?> childDescs;
-					if((propDesc = delegator.getPropertyDescriptor(parent, ref)) != null) {
-						if(propDesc.getChoiceOfValues(parent).containsAll(collection)) {					
-							possibleReferences.add(ref);
+					if ((propDesc = delegator.getPropertyDescriptor(parent, ref)) != null) {
+
+						if (!propDesc.getChoiceOfValues(parent).containsAll(collection)) {
+							possibleReferences.put(ref, false);
+						} else {
+
+							// Check if the objects in the 'collection' are already referenced by 'parent.ref'
+							//
+							List<Object> currentValues = new ArrayList<>(ref.isMany()
+									? (Collection<Object>) parent.eGet(ref) : Arrays.asList(parent.eGet(ref)));
+
+							possibleReferences.put(ref,
+									!ref.isUnique() || collection.parallelStream().noneMatch(currentValues::contains));
 						}
+
 						// ... otherwise, we evaluate the 'NewChildDescriptors'.
 						//
-					} else if(!(delegator.getNewChildDescriptors(parent, domain, null)).isEmpty()) {
+					} else if (!delegator.getNewChildDescriptors(parent, domain, null).isEmpty()) {
 						childDescs = delegator.getNewChildDescriptors(parent, domain, null);
-						
-						Iterator<CommandParameter> it = (Iterator<CommandParameter>) childDescs.parallelStream().filter(c -> c instanceof CommandParameter 
-								&& ref.equals(((CommandParameter) c).getEStructuralFeature()) && ((CommandParameter) c).getEValue() != null).collect(Collectors.toList()).iterator();
-						
-						// Check that there is a child descriptor for the required 'ref' and the set of required 'EClasses'
+
+						Iterator<CommandParameter> it = (Iterator<CommandParameter>) childDescs.parallelStream()
+								.filter(c -> c instanceof CommandParameter
+										&& ref.equals(((CommandParameter) c).getEStructuralFeature())
+										&& ((CommandParameter) c).getEValue() != null)
+								.collect(Collectors.toList()).iterator();
+
+						// Check that there is a child descriptor for the required 'ref' and the set of required
+						// 'EClasses'
 						//
 						Set<EClass> eClasses = new HashSet<>();
-						while(it.hasNext()) {
+						while (it.hasNext()) {
 							eClasses.add(it.next().getEValue().eClass());
 						}
-						
-						if(!eClasses.isEmpty() && eClassSet.containsAll(eClasses)) {
-							possibleReferences.add(ref);
+
+						if (!eClasses.isEmpty() && eClasses.containsAll(eClassSet)) {
+							possibleReferences.put(ref, domain
+									.createCommand(AddCommand.class, new CommandParameter(parent, ref, collection))
+									.canExecute());
 						}
-						
+
 					}
-					
+
 				}
 			}
 		} else {
-			//TODO should we apply another strategy in case the delegator could not be determined?
+			// TODO should we apply another strategy in case the delegator could not be determined?
 		}
-		
+
 		ArrayList<AbstractCommand> commands = new ArrayList<>();
-		if(dragAndDropCommand.canExecute()) {
-			commands.add((AbstractCommand) dragAndDropCommand);
+		if (dragAndDropCommand.canExecute()) {
+			commands.add(dragAndDropCommand);
 		}
-		
+
 		// Create a suitable drag and drop command depending on the number of possibilities
 		//
-		if(possibleReferences.isEmpty()) {
-			return dragAndDropCommand;			
-		} else if(possibleReferences.size() == 1) {
-			return createDragAndDropCommand(domain, (Collection<EObject>) collection, parent, possibleReferences.iterator().next());
+		if (possibleReferences.isEmpty() || possibleReferences.values().parallelStream().allMatch(v -> !v)) {
+			return dragAndDropCommand;
+		} else if (possibleReferences.size() == 1) {
+			return this.createDragAndDropCommand(domain, (Collection<EObject>) collection, parent,
+					possibleReferences.keySet().iterator().next());
 		} else {
-			for (EReference ref : possibleReferences) {
-				commands.add(createDragAndDropCommand(domain, (Collection<EObject>) collection, parent, ref));
+			for (EReference ref : possibleReferences.keySet()) {
+				commands.add(this.createDragAndDropCommand(domain, (Collection<EObject>) collection, parent, ref));
 			}
 		}
-		
-		if(commands.size() == 1) {
+
+		if (commands.size() == 1) {
 			return commands.get(0);
 		} else {
-			return new AmbiguousDragAndDropCommandWrapper(owner, location, operations,
-					operation, collection, commands, strategy == null ? new ICommandSelectionStrategy() {} : strategy);
+			return new AmbiguousDragAndDropCommandWrapper(owner, location, operations, operation, collection, commands,
+					strategy == null ? new ICommandSelectionStrategy() {
+					} : strategy);
 		}
 	}
 
 	/**
-	 * Based on the type of reference to set, creates either a {@link BasicDragAndDropSetCommand}, a 
+	 * Based on the type of reference to set, creates either a {@link BasicDragAndDropSetCommand}, a
 	 * {@link BasicDragAndDropAddCommand}, or a {@link BasicDragAndDropCompoundCommand} (in case the collection needs to
 	 * be removed from an old feature first).
-	 * 
-	 * @param domain The {@link EditingDomain} that will be used to execute the command.
-	 * @param collection The dragged elements.
-	 * @param parent The {@link EObject} on that the command will be executed.
-	 * @param ref The {@link EReference} that to that the collection of objects shall be added/that shall be set.
+	 *
+	 * @param domain
+	 *            The {@link EditingDomain} that will be used to execute the command.
+	 * @param collection
+	 *            The dragged elements.
+	 * @param parent
+	 *            The {@link EObject} on that the command will be executed.
+	 * @param ref
+	 *            The {@link EReference} that to that the collection of objects shall be added/that shall be set.
 	 * @return The drag and drop command.
 	 */
-	default AbstractCommand createDragAndDropCommand(EditingDomain domain, Collection<EObject> collection, EObject parent,
-			EReference ref) {
-		
-		if(ref.isContainment()) {
+	default AbstractCommand createDragAndDropCommand(EditingDomain domain, Collection<EObject> collection,
+			EObject parent, EReference ref) {
+
+		if (ref.isContainment()) {
 			return new DragAndDropChangeContainingFeatureCommand(parent, ref, collection);
 		} else {
-			if(ref.isMany()) {
+			if (ref.isMany()) {
 				return new BasicDragAndDropAddCommand(domain, parent, ref, collection);
 			} else {
 				return new BasicDragAndDropSetCommand(domain, parent, ref, collection.iterator().next(), 0);
 			}
 		}
 	}
-	
+
 	/**
-	 * This returns the {@link ICommandSelectionStrategy} that is used by 
+	 * This returns the {@link ICommandSelectionStrategy} that is used by
 	 * {@link #createCustomDragAndDropCommand(EditingDomain, Object, float, int, int, Collection, ICommandSelectionStrategy)}.
-	 * 
-	 * @return The {@link ICommandSelectionStrategy} to be used by 
-	 * {@link #createCustomDragAndDropCommand(EditingDomain, Object, float, int, int, Collection, ICommandSelectionStrategy)}.
+	 *
+	 * @return The {@link ICommandSelectionStrategy} to be used by
+	 *         {@link #createCustomDragAndDropCommand(EditingDomain, Object, float, int, int, Collection, ICommandSelectionStrategy)}.
 	 */
 	public default ICommandSelectionStrategy getCommandSelectionStrategy() {
-		return new ICommandSelectionStrategy() {};
+
+		return new ICommandSelectionStrategy() {
+		};
 	}
-	
+
 }
--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/commands/AmbiguousDragAndDropCommandWrapper.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/commands/AmbiguousDragAndDropCommandWrapper.java
@@ -7,90 +7,100 @@ import java.util.List;
 import org.eclipse.emf.common.command.AbstractCommand;
 import org.eclipse.emf.common.command.UnexecutableCommand;
 import org.eclipse.emf.edit.command.DragAndDropFeedback;
+
 import de.tud.et.ifa.agtele.emf.edit.ICommandSelectionStrategy;
 import de.tud.et.ifa.agtele.emf.exceptions.SorryNoOtherWorkaroundException;
 
 /**
- * This represents an <em>unambiguous</em> command. This means that this {@link AbstractCommand} wraps a list of other 
- * commands. Before executing, one command is selected from the list based on the specified {@link ICommandSelectionStrategy}.
- * Furthermore, this provides {@link DragAndDropFeedback} and can thus be used as DragAndDropCommand.
+ * This represents an <em>unambiguous</em> command. This means that this {@link AbstractCommand} wraps a list of other
+ * commands. Before executing, one command is selected from the list based on the specified
+ * {@link ICommandSelectionStrategy}. Furthermore, this provides {@link DragAndDropFeedback} and can thus be used as
+ * DragAndDropCommand.
  */
-public class AmbiguousDragAndDropCommandWrapper extends AbstractCommand implements
-		DragAndDropFeedback {
+public class AmbiguousDragAndDropCommandWrapper extends AbstractCommand implements DragAndDropFeedback {
 
 	/**
 	 * The (ambiguous) list of commands that this wraps.
 	 */
 	protected ArrayList<AbstractCommand> commands;
-	
+
 	/**
 	 * The (ambiguous) list of commands that are currently {@link #validate(Object, float, int, int, Collection) valid}.
 	 */
 	protected ArrayList<AbstractCommand> validCommands = new ArrayList<>();
-	
+
 	/**
 	 * The single, unambiguous command after resolving the ambiguities.
 	 */
 	protected AbstractCommand command;
-	
+
 	/**
 	 * The {@link ICommandSelectionStrategy} that shall be used to select a single command to execute.
 	 */
 	protected ICommandSelectionStrategy strategy;
-	
+
 	/**
 	 * This creates an instance.
-	 * @param owner The object on that the command will be executed.
-	 * @param location The location (between 0.0 and 1.0) in relation to the '<em>owner</em>' where the command will be executed.
-	 * @param operations The permitted operations.
-	 * @param operation The desired operation.
-	 * @param collection The dragged elements.
-	 * @param commands The ambiguous list of {@link AbstractCommand commands} that this shall wrap.
-	 * @param strategy The {@link ICommandSelectionStrategy} that shall be applied to select an unambiguous 
-	 * command before execution.
+	 *
+	 * @param owner
+	 *            The object on that the command will be executed.
+	 * @param location
+	 *            The location (between 0.0 and 1.0) in relation to the '<em>owner</em>' where the command will be
+	 *            executed.
+	 * @param operations
+	 *            The permitted operations.
+	 * @param operation
+	 *            The desired operation.
+	 * @param collection
+	 *            The dragged elements.
+	 * @param commands
+	 *            The ambiguous list of {@link AbstractCommand commands} that this shall wrap.
+	 * @param strategy
+	 *            The {@link ICommandSelectionStrategy} that shall be applied to select an unambiguous command before
+	 *            execution.
 	 */
-	public AmbiguousDragAndDropCommandWrapper(Object owner, float location, int operations,
-			int operation, Collection<?> collection, List<AbstractCommand> commands, ICommandSelectionStrategy strategy) {
-		
+	public AmbiguousDragAndDropCommandWrapper(Object owner, float location, int operations, int operation,
+			Collection<?> collection, List<AbstractCommand> commands, ICommandSelectionStrategy strategy) {
+
 		this.commands = commands == null ? new ArrayList<>() : (ArrayList<AbstractCommand>) commands;
-		
-		if(commands == null || commands.isEmpty()) {
+
+		if (commands == null || commands.isEmpty()) {
 			this.command = UnexecutableCommand.INSTANCE;
-		} else if(commands.size() == 1) {
+		} else if (commands.size() == 1) {
 			this.command = commands.iterator().next();
 		} else {
 			this.command = null;
 		}
-		
+
 		this.strategy = strategy;
-		
+
 		/*
-		 * we need to validate the command already in the constructor as it seems that a new command is created directly before
-		 * execution that would not get validated otherwise
+		 * we need to validate the command already in the constructor as it seems that a new command is created directly
+		 * before execution that would not get validated otherwise
 		 */
-		validate(owner, location, operations, operation, collection);
+		this.validate(owner, location, operations, operation, collection);
 	}
 
 	@Override
-	public boolean validate(Object owner, float location, int operations,
-			int operation, Collection<?> collection) {
-		
-		validCommands.clear();
-		for (AbstractCommand c : commands) {
-			if(c instanceof DragAndDropFeedback && ((DragAndDropFeedback) c).validate(owner, location, operations, operation, collection)) {
-				validCommands.add(c);
+	public boolean validate(Object owner, float location, int operations, int operation, Collection<?> collection) {
+
+		this.validCommands.clear();
+		for (AbstractCommand c : this.commands) {
+			if (c instanceof DragAndDropFeedback
+					&& ((DragAndDropFeedback) c).validate(owner, location, operations, operation, collection)) {
+				this.validCommands.add(c);
 			}
 		}
-		
-		return !validCommands.isEmpty();
+
+		return !this.validCommands.isEmpty();
 	}
-	
+
 	@Override
 	public boolean canExecute() {
-		
+
 		// This can execute if at least one of the wrapped commands can execute
-		for (AbstractCommand c : commands) {
-			if(c.canExecute()) {
+		for (AbstractCommand c : this.commands) {
+			if (c.canExecute()) {
 				return true;
 			}
 		}
@@ -99,67 +109,71 @@ public class AmbiguousDragAndDropCommandWrapper extends AbstractCommand implemen
 
 	@Override
 	public int getFeedback() {
-		
+
 		// Determine the feedback to be shown based on the number and types of 'validCommands'
-		if(validCommands.isEmpty()) {
-			return FEEDBACK_NONE;
-		} else if(validCommands.size() == 1 && validCommands.get(0) instanceof DragAndDropFeedback) {
-			return ((DragAndDropFeedback) (validCommands.get(0))).getFeedback();
+		if (this.validCommands.isEmpty()) {
+			return DragAndDropFeedback.FEEDBACK_NONE;
+		} else if (this.validCommands.size() == 1 && this.commands.size() == 1
+				&& this.validCommands.get(0) instanceof DragAndDropFeedback) {
+			return ((DragAndDropFeedback) this.validCommands.get(0)).getFeedback();
 		} else {
-			return FEEDBACK_SELECT;
+			return DragAndDropFeedback.FEEDBACK_SELECT;
 		}
 	}
 
 	@Override
 	public int getOperation() {
-		
+
 		// Determine the operation based on the number and types of 'validCommands'
-		if(validCommands.isEmpty()) {
-			return DROP_NONE;
-		} else if(validCommands.size() == 1 && validCommands.get(0) instanceof DragAndDropFeedback) {
-			return ((DragAndDropFeedback) (validCommands.get(0))).getOperation();
+		if (this.validCommands.isEmpty()) {
+			return DragAndDropFeedback.DROP_NONE;
+		} else if (this.validCommands.size() == 1 && this.commands.size() == 1
+				&& this.validCommands.get(0) instanceof DragAndDropFeedback) {
+			return ((DragAndDropFeedback) this.validCommands.get(0)).getOperation();
 		} else {
-			return DROP_LINK;
+			return DragAndDropFeedback.DROP_LINK;
 		}
 	}
 
 	@Override
 	public void execute() {
 
-		if(command == null) {
-			if(validCommands.isEmpty()) {
-				command = null;
-			} else if(validCommands.size() == 1) {
-				command = validCommands.get(0);
+		if (this.command == null) {
+			if (this.validCommands.isEmpty()) {
+				this.command = null;
+				// } else if(validCommands.size() == 1) {
+				// command = validCommands.get(0);
 			} else {
-				command = strategy.selectCommandToExecute(validCommands);				
+				this.command = this.strategy.selectCommandToExecute(this.commands);
 			}
-			validCommands.clear();
+			this.validCommands.clear();
 		}
-		
-		if(command == null) {
-			//We have to throw an exception in order to avoid the editor to be marked as dirty. Yes, this will pollute the console, but...
+
+		if (this.command == null) {
+			// We have to throw an exception in order to avoid the editor to be marked as dirty. Yes, this will pollute
+			// the console, but...
 			throw new SorryNoOtherWorkaroundException();
 		}
-		
-		command.execute();
+
+		this.command.execute();
 	}
 
 	@Override
 	public void redo() {
-		if(command != null) {
-			command.redo();
+
+		if (this.command != null) {
+			this.command.redo();
 		}
 	}
-	
+
 	@Override
 	public void undo() {
-		if(command != null) {
-			command.undo();
+
+		if (this.command != null) {
+			this.command.undo();
 		} else {
 			super.undo();
 		}
 	}
-	
 
 }
