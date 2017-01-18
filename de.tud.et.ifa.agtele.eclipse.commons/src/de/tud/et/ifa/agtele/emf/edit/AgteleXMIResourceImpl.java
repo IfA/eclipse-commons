4b152f29dd26fef8fb68f6fef06fa4094c8f1cf9
Lukas Baron <lukas.baron@tu-dresden.de>
2017-01-18 16:02:05 +0100

    adds classes in order to introduce the possibility to set options for the resource in model editors

---
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/AgteleXMIResourceImpl.java
@@ -0,0 +1,43 @@
+package de.tud.et.ifa.agtele.emf.edit;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.xmi.XMIResource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+
+/**
+ * The agtele XMIResource will be used by the
+ * {@link AgteleAdapterFactoryEditingDomain} in order to replace a standard
+ * {@link XMIResource}.
+ *
+ * @author Baron
+ *
+ */
+public class AgteleXMIResourceImpl extends XMIResourceImpl implements IAgteleResource {
+
+	protected boolean useUUIDs = false;
+
+	public AgteleXMIResourceImpl(URI uri) {
+		super(uri);
+	}
+
+	@Override
+	public boolean isSaveUsingUUIDs() {
+		return this.useUUIDs;
+	}
+
+	@Override
+	public void setSaveUsingUUIDs(boolean useUUIDs) {
+		this.useUUIDs = useUUIDs;
+	}
+
+	@Override
+	protected boolean useUUIDs() {
+		return this.useUUIDs;
+	}
+
+	@Override
+	protected boolean useIDAttributes() {
+		// TODO Auto-generated method stub
+		return super.useIDAttributes();
+	}
+}
