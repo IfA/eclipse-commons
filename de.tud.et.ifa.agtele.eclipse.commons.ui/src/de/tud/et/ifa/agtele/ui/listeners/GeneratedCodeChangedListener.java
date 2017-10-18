77638b5bf6b491fe5299f5db29ec55e84cf6f550
mfreund <matthias.freund@tu-dresden.de>
2017-10-18 14:35:35 +0200

    Enable different modes in the listener
    USER -> let the user decide whether to add the NOT tag
    AUTOMATIC -> automatically add NOT tags
    OFF -> listener is turned off
    
    Also, create an 'init' method that binds the listener to the appropriate
    workbench events.

--- de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/listeners/GeneratedCodeChangedListener.java
+++ de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/listeners/GeneratedCodeChangedListener.java
@@ -36,6 +36,7 @@ import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.IMessageProvider;
 import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.window.Window;
@@ -48,7 +49,10 @@ import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchCommandConstants;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
 
 import de.tud.et.ifa.agtele.ui.AgteleUIPlugin;
 import de.tud.et.ifa.agtele.ui.util.UIHelper;
@@ -58,14 +62,40 @@ import de.tud.et.ifa.agtele.ui.util.UIHelper;
  * generated Java code (tagged with '@generated') was changed by the user, this reminds the user to change the
  * annotation to '@generated NOT'.
  * <p />
+ * Note: The listener has to be initialized via {@link #init()}.
+ * <p />
  * Note: Currently, only changes to generated methods are addressed. However, it could maybe be useful to also handled
  * changes to fields or class declarations in the future.
+ * <p />
+ * Note: The listener can work in various {@link GeneratedCodeChangedListenerMode modes} that can be enabled via
+ * {@link #setMode(GeneratedCodeChangedListenerMode)}. Specified modes are persisted in the {@link IPreferenceStore} of
+ * the {@link AgteleUIPlugin} by use of the {@link #OPERATING_MODE_SETTING_KEY}.
  *
  *
  * @author mfreund
  */
 @SuppressWarnings("restriction")
 public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
+
+	/**
+	 * The singleton instance of this listener.
+	 */
+	protected static GeneratedCodeChangedListener instance;
+
+	/**
+	 * The {@link GeneratedCodeChangedListenerMode} that the listener currently operates on.
+	 */
+	private static GeneratedCodeChangedListenerMode mode;
+
+	/**
+	 * The {@link IPreferenceStore} of the {@link AgteleUIPlugin}.
+	 */
+	protected static IPreferenceStore store = null;
+
+	/**
+	 * The settings key for the {@link IPreferenceStore}.
+	 */
+	protected static final String OPERATING_MODE_SETTING_KEY = "GENERATED_CODE_CHANGED_LISTENER_OPERATING_MODE";
 
 	/**
 	 * A regex that can be used to check if the JavaDoc of a method is equipped with the '@generated' tag. Also, it can
@@ -83,10 +113,32 @@ public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
 			.compile(GeneratedCodeChangedListener.JAVADOC_WITH_GENERATED_TAG_REGEX);
 
 	/**
+	 * Initializes the singleton {@link #instance} of this listener and registers it to the {@link ICommandService}.
+	 *
+	 */
+	public static void init() {
+
+		if (GeneratedCodeChangedListener.instance != null) {
+
+			// The listener was already initialized
+			//
+			return;
+		}
+
+		GeneratedCodeChangedListener.instance = new GeneratedCodeChangedListener();
+
+		IWorkbench wb = PlatformUI.getWorkbench();
+
+		final ICommandService commandService = wb.getService(ICommandService.class);
+
+		commandService.addExecutionListener(new GeneratedCodeChangedListener());
+	}
+
+	/**
 	 * This creates an instance.
 	 *
 	 */
-	public GeneratedCodeChangedListener() {
+	protected GeneratedCodeChangedListener() {
 
 		// Only listen to 'save' events in the Java editor
 		//
@@ -100,6 +152,10 @@ public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
 	 */
 	@Override
 	protected void doPreExecute(String commandId, ExecutionEvent event) {
+
+		if (GeneratedCodeChangedListener.getMode() == GeneratedCodeChangedListenerMode.OFF) {
+			return;
+		}
 
 		IEditorPart editor = UIHelper.getCurrentEditor();
 
@@ -315,18 +371,30 @@ public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
 
 			SourceMethod sourceMethod = methods.get(i);
 
-			AddNotToGeneratedTagDialog dialog = new AddNotToGeneratedTagDialog(sourceMethod.getElementName(),
-					methods.size() - i - 1);
-			dialog.create();
+			// Assume we are in GeneratedCodeChangedListenerMode#AUTOMATIC mode
+			//
+			boolean addNotTag = true;
+			Optional<String> explanation = Optional.empty();
+			boolean doNotAskAnyMore = true;
 
-			int result = dialog.open();
+			if (GeneratedCodeChangedListener.getMode() == GeneratedCodeChangedListenerMode.USER) {
 
-			Optional<String> explanation = dialog.getAddNotTagExplanation();
-			boolean doNotAskAnyMore = dialog.isDoNotAskAnyMore();
+				// If we are instead in USER mode, present a dialog to the user
+				//
+				AddNotToGeneratedTagDialog dialog = new AddNotToGeneratedTagDialog(sourceMethod.getElementName(),
+						methods.size() - i - 1);
+				dialog.create();
+
+				int result = dialog.open();
+
+				explanation = dialog.getAddNotTagExplanation();
+				doNotAskAnyMore = dialog.isDoNotAskAnyMore();
+				addNotTag = result == Window.OK;
+			}
 
 			// The user chose 'No' -> do nothing
 			//
-			if (result != Window.OK) {
+			if (!addNotTag) {
 				if (doNotAskAnyMore) {
 					break;
 				} else {
@@ -395,6 +463,56 @@ public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
 	}
 
 	/**
+	 * @return the {@link #mode}
+	 */
+	public static GeneratedCodeChangedListenerMode getMode() {
+
+		if (GeneratedCodeChangedListener.mode == null) {
+			// Read the mode from the preferences
+			//
+			if (GeneratedCodeChangedListener.store == null) {
+				GeneratedCodeChangedListener.store = AgteleUIPlugin.getPlugin().getPreferenceStore();
+			}
+			int storedModeValue = GeneratedCodeChangedListener.store
+					.getInt(GeneratedCodeChangedListener.OPERATING_MODE_SETTING_KEY);
+			GeneratedCodeChangedListenerMode storedMode = GeneratedCodeChangedListenerMode.getMode(storedModeValue);
+
+			GeneratedCodeChangedListener.mode = storedMode != null ? storedMode : GeneratedCodeChangedListenerMode.USER;
+		}
+
+		return GeneratedCodeChangedListener.mode;
+	}
+
+	/**
+	 * This sets the new {@link GeneratedCodeChangedListenerMode}.
+	 *
+	 * @param mode
+	 *            the {@link GeneratedCodeChangedListenerMode} to set
+	 */
+	public static void setMode(GeneratedCodeChangedListenerMode mode) {
+
+		if (GeneratedCodeChangedListener.store == null) {
+			GeneratedCodeChangedListener.store = AgteleUIPlugin.getPlugin().getPreferenceStore();
+		}
+
+		GeneratedCodeChangedListener.mode = mode;
+		GeneratedCodeChangedListener.store.setValue(GeneratedCodeChangedListener.OPERATING_MODE_SETTING_KEY,
+				mode.getValue());
+	}
+
+	/**
+	 * This sets the new {@link GeneratedCodeChangedListenerMode}.
+	 *
+	 * @param modeValue
+	 *            the value of the {@link GeneratedCodeChangedListenerMode} to set
+	 */
+	public static void setMode(int modeValue) {
+
+		GeneratedCodeChangedListener.setMode(GeneratedCodeChangedListenerMode.getMode(modeValue));
+
+	}
+
+	/**
 	 * An enum describing the different operating modes of the {@link GeneratedCodeChangedListener}.
 	 *
 	 * @author mfreund
@@ -431,6 +549,28 @@ public class GeneratedCodeChangedListener extends WorkspaceCommandListener {
 		public int getValue() {
 
 			return this.value;
+		}
+
+		/**
+		 * Returns the mode for the specified value.
+		 *
+		 * @param value
+		 *            the int value of the mode to return
+		 * @return The {@link GeneratedCodeChangedListenerMode} or '<em>null</em>' if no mode for the specified value
+		 *         exists.
+		 */
+		public static GeneratedCodeChangedListenerMode getMode(int value) {
+
+			if (value == 0) {
+				return USER;
+			} else if (value == 1) {
+				return GeneratedCodeChangedListenerMode.AUTOMATIC;
+			} else if (value == 2) {
+				return GeneratedCodeChangedListenerMode.OFF;
+			} else {
+				throw new IllegalArgumentException(
+						"The value " + value + " does not represent a valid GeneratedCodeChangedListenerMode!");
+			}
 		}
 	}
 
