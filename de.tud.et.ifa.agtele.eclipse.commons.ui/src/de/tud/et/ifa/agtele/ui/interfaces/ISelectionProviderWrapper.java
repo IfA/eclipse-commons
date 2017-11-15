047bae3e2427d99bf4b78a068f3a9f25f0b1d033
mfreund <matthias.freund@tu-dresden.de>
2017-11-15 10:41:22 +0100

    Create new interface ISelectionProviderWrapper
    This interface can be implemented by classes that provide the
    ISelectionProvider functionality by redirecting to one or multiple other
    ISelectionProviders (e.g. contained viewers) instead of providing a
    selection on their own.

---
+++ de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/interfaces/ISelectionProviderWrapper.java
@@ -0,0 +1,62 @@
+/**
+ *
+ */
+package de.tud.et.ifa.agtele.ui.interfaces;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.StructuredSelection;
+import org.eclipse.jface.viewers.Viewer;
+
+/**
+ * This interface can be implemented by classes that provide the {@link ISelectionProvider} functionality by redirecting
+ * to one or multiple other {@link ISelectionProvider providers} (e.g. contained {@link Viewer viewers}) instead of
+ * providing a selection on their own.
+ * <p />
+ * The default implementations of the various interface methods of {@link ISelectionProvider} just redirect to the
+ * implementations in all of the {@link #getWrappedProviders() wrapped providers}.
+ *
+ * @author mfreund
+ */
+public interface ISelectionProviderWrapper extends ISelectionProvider {
+
+	/**
+	 * This returns the collection of {@link ISelectionProvider ISelectionProviders} wrapped by this.
+	 *
+	 * @return The wrapped {@link ISelectionProvider ISelectionProviders}.
+	 */
+	public abstract Collection<ISelectionProvider> getWrappedProviders();
+
+	@Override
+	public default void addSelectionChangedListener(ISelectionChangedListener listener) {
+
+		this.getWrappedProviders().stream().forEach(p -> p.addSelectionChangedListener(listener));
+	}
+
+	@Override
+	public default ISelection getSelection() {
+
+		return new StructuredSelection(
+				this.getWrappedProviders().stream().filter(p -> p.getSelection() instanceof IStructuredSelection)
+						.flatMap(p -> Arrays.asList(((IStructuredSelection) p.getSelection()).toArray()).stream())
+						.collect(Collectors.toList()));
+	}
+
+	@Override
+	public default void removeSelectionChangedListener(ISelectionChangedListener listener) {
+
+		this.getWrappedProviders().stream().forEach(p -> p.removeSelectionChangedListener(listener));
+	}
+
+	@Override
+	public default void setSelection(ISelection selection) {
+
+		this.getWrappedProviders().stream().forEach(p -> p.setSelection(selection));
+	}
+}
