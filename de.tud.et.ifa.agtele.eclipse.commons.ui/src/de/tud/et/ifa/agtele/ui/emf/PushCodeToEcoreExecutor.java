ead55bcbcf95c29977c4a2af03685a2ed433264b
Lukas Baron <lukas.baron@tu-dresden.de>
2018-10-10 17:31:53 +0200

    Fixes the compilation of the java code to push in order to catch imported types used as constructor: new <%qualified-name%>()

--- de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/emf/PushCodeToEcoreExecutor.java
+++ de.tud.et.ifa.agtele.eclipse.commons.ui/src/de/tud/et/ifa/agtele/ui/emf/PushCodeToEcoreExecutor.java
@@ -466,7 +466,7 @@ public class PushCodeToEcoreExecutor {
 			String packageName = importDeclaration.substring(0, lastDotIndex);
 			String typeName = importDeclaration.substring(lastDotIndex + 1);
 
-			compiledCode = compiledCode.replaceFirst("([\\(\\[\\{\\s<])" + typeName + "([\\s\\)<>:\\.,])",
+			compiledCode = compiledCode.replaceFirst("([\\(\\[\\{\\s<\\u0020\\p{javaSpaceChar}])" + typeName + "([\\s\\)\\(<>:\\.,])",
 					"$1<%" + packageName + "." + typeName + "%>$2");
 		}
 
