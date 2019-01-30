14fbae68e0b9f7aa1a7a660ab0aadf78059e4983
Lukas Baron <lukas.baron@tu-dresden.de>
2019-01-30 10:31:40 +0100

    adds custom label provider that can be used in order to make the display of the model structure more expressive

---
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/AgteleStyledLabelProvider.java
@@ -0,0 +1,61 @@
+package de.tud.et.ifa.agtele.emf.edit;
+
+import org.eclipse.emf.common.EMFPlugin;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+
+public class AgteleStyledLabelProvider extends DelegatingStyledCellLabelProvider {
+
+	private EMFPlugin editPlugin = null;
+	
+	public AgteleStyledLabelProvider(IStyledLabelProvider labelProvider, EMFPlugin emfPlugin) {
+		this(labelProvider);
+		this.editPlugin = emfPlugin;
+	}
+
+	public AgteleStyledLabelProvider(IStyledLabelProvider labelProvider) {
+		super(labelProvider);
+	}
+
+	public String getReferenceNameSeparator () {
+		return "\u0020\u00bb\u0020";
+	}
+	
+	public String getFeatureLabel (EStructuralFeature feature) {
+		if (this.editPlugin != null) {
+			return this.editPlugin.getString(
+				"_UI_" + feature.getEContainingClass().getName()
+				+ "_" + feature.getName() + "_feature");
+		}
+		return feature.getName();
+	}
+	
+	@Override
+	protected StyledString getStyledText(Object element) {
+		StyledString typeString = super.getStyledText(element);
+		StyledString result;
+		if (element instanceof XMIResourceImpl) {
+			return typeString;
+		}
+		if (element instanceof MinimalEObjectImpl && ((MinimalEObjectImpl)element).eContainer() == null) {
+			result = typeString;
+		} else if (element instanceof MinimalEObjectImpl) {
+			EObject e = (EObject) element;
+			try {
+				result = new StyledString(
+						this.getFeatureLabel(e.eContainingFeature())
+						+ this.getReferenceNameSeparator(),
+					StyledString.QUALIFIER_STYLER).append(typeString);
+			} catch (Exception ex) {
+				result = typeString;
+			}
+		} else {
+			return typeString;
+		}
+		return result;
+	}
+}
