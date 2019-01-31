ca2ab67f9b0782bc8090be68bcd19fe1ecf10555
Lukas Baron <lukas.baron@tu-dresden.de>
2019-01-31 16:23:28 +0100

    extending the referencing identification string provider interface

--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IReferencingIdentificationStringProvider.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IReferencingIdentificationStringProvider.java
@@ -1,5 +1,14 @@
 package de.tud.et.ifa.agtele.emf.edit;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.eclipse.emf.ecore.EObject;
+
 public interface IReferencingIdentificationStringProvider {
 	String getReferencingIdentificationString (Object element);
+
+	default Collection<String> getReferencingIdentificationStrings(EObject eObject) {
+		return Collections.singleton(this.getReferencingIdentificationString(eObject));
+	}
 }
