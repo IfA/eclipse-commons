8260da623636cc2d1b83df18db7a0b259996e7f4
Lukas Baron <lukas.baron@tu-dresden.de>
2019-02-14 11:23:00 +0100

    adds some utility

--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IReferencingIdentificationStringProvider.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/edit/IReferencingIdentificationStringProvider.java
@@ -12,4 +12,34 @@ public interface IReferencingIdentificationStringProvider {
 	};
 
 	Collection<String> getReferencingIdentificationStrings(Object element);
+	
+	
+	static String getUriPrefix (String uri) {
+		if (uri.contains(":") && (uri.length() > (uri.indexOf(":") + 1)) && uri.indexOf(":") > 0) {
+			int index = uri.indexOf(":");
+			if (uri.charAt(index + 1) == '/') {
+				return null;
+			}
+			if (Character.isDigit(uri.charAt(index + 1))) {
+				if (uri.indexOf("/", index) > -1) {
+					if (uri.substring(index +1, uri.indexOf("/", index)).matches("{\\d}+")) {
+						return null;
+					}
+				}
+			}
+			return uri.substring(0, index);
+		}
+		return null;		
+	}
+	
+	static boolean hasUriPrefix (String uri) {
+		return IReferencingIdentificationStringProvider.getUriPrefix(uri) != null;
+	}
+	
+	static String removeUriPrefix (String uri) {
+		if (IReferencingIdentificationStringProvider.hasUriPrefix(uri)) {
+			return uri.substring(uri.indexOf(":") +1);
+		}
+		return uri;
+	}
 }
