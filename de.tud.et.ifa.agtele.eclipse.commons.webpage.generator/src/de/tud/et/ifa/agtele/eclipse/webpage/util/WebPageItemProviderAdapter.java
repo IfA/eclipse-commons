947373169e25b672d03e0a75246b6f88713ff00a
Lukas Baron <lukas.baron@tu-dresden.de>
2020-08-28 00:35:44 +0200

    design fixes, repo cleanup

--- de.tud.et.ifa.agtele.eclipse.commons.webpage.generator/src/de/tud/et/ifa/agtele/eclipse/webpage/util/WebPageItemProviderAdapter.java
+++ de.tud.et.ifa.agtele.eclipse.commons.webpage.generator/src/de/tud/et/ifa/agtele/eclipse/webpage/util/WebPageItemProviderAdapter.java
@@ -3,9 +3,11 @@ package de.tud.et.ifa.agtele.eclipse.webpage.util;
 import org.eclipse.emf.common.notify.AdapterFactory;
 
 import de.tud.et.ifa.agtele.emf.edit.CommonItemProviderAdapter;
+import de.tud.et.ifa.agtele.emf.edit.ICommandSelectionStrategy;
 import de.tud.et.ifa.agtele.emf.edit.IDragAndDropProvider;
 import de.tud.et.ifa.agtele.emf.edit.IRequireRelatedModelUpdateProvider;
 import de.tud.et.ifa.agtele.help.IEMFModelHelpItemProvider;
+import de.tud.et.ifa.agtele.ui.emf.edit.UserChoiceCommandSelectionStrategy;
 
 public class WebPageItemProviderAdapter extends CommonItemProviderAdapter
 	implements IEMFModelHelpItemProvider, IDragAndDropProvider, IRequireRelatedModelUpdateProvider {
@@ -13,4 +15,10 @@ public class WebPageItemProviderAdapter extends CommonItemProviderAdapter
 	public WebPageItemProviderAdapter(AdapterFactory adapterFactory) {
 		super(adapterFactory);
 	}
+
+	@Override
+	public ICommandSelectionStrategy getCommandSelectionStrategy() {
+		return new UserChoiceCommandSelectionStrategy();
+	}
+	
 }
