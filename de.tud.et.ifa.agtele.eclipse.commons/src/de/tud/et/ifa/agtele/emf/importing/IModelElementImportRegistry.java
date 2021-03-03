a0a4f338c43fdbac95d8695c70c3a807650fd616
Lukas Baron <lukas.baron@tu-dresden.de>
2021-03-03 20:44:02 +0100

    modifies import registry in order to also support imports with multiple imported elements per original
    TODO untested

--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/importing/IModelElementImportRegistry.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/importing/IModelElementImportRegistry.java
@@ -1,15 +1,23 @@
 package de.tud.et.ifa.agtele.emf.importing;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 
 public interface IModelElementImportRegistry {
+	
 	void registerImportedElement (Object original, EObject imported);
+	void registerImportedElementUnique (Object original, EObject imported);
 	EObject getImportedElement (Object original);
+	Set<EObject> getImportedElements(Object original);
+	EObject getImportedElement (Resource res, Object original);
 	Object getOriginalElement (EObject imported);
 	
-	void deregister (Object createdOrOrigninal);
+	void deregister (Object createdOrOriginal);
 	
 	Collection<EObject> getImportedElements ();
+	Collection<Object> getOriginalElements ();
 }
--- de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/importing/ModelElementImportRegistry.java
+++ de.tud.et.ifa.agtele.eclipse.commons/src/de/tud/et/ifa/agtele/emf/importing/ModelElementImportRegistry.java
@@ -1,29 +1,48 @@
 package de.tud.et.ifa.agtele.emf.importing;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
 
 public class ModelElementImportRegistry implements IModelElementImportRegistry {
 
-	protected Map<Object,EObject> importedObjects = new HashMap<>();
+	protected Map<Object,LinkedHashSet<EObject>> importedObjects = new HashMap<>(); //TODO there may be multiple imported objects per aas element
 	protected Map<EObject,Object> originalNodes = new HashMap<>();
 	
 	
 	@Override
 	public void registerImportedElement(Object original, EObject imported) {
-		this.importedObjects.put(original, imported);
+		LinkedHashSet<EObject> set = importedObjects.get(original);
+		if (set == null) {
+			set = new LinkedHashSet<>();
+			importedObjects.put(original, set);
+		}
+		set.add(imported);
 		this.originalNodes.put(imported, original);
 	}
-
+	@Override
+	public void registerImportedElementUnique(Object original, EObject imported) {
+		this.deregister(original);
+		this.registerImportedElement(original, imported);
+	}
 
 	@Override
 	public EObject getImportedElement(Object original) {
-		return this.importedObjects.get(original);
+		Set<EObject> set = importedObjects.get(original);
+		if (set != null) {
+			return (new ArrayList<EObject>(set)).get(set.size()-1);
+		}
+		
+		return null;
 	}
-
 
 	@Override
 	public Object getOriginalElement(EObject imported) {
@@ -31,28 +50,78 @@ public class ModelElementImportRegistry implements IModelElementImportRegistry {
 	}
 
 
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-	public void deregister(Object createdOrOrigninal) {
+	public void deregister(Object createdOrOriginal) {
 		Object original = null;
-		EObject imported = null;
+		Set<EObject> imported = null;
+		EObject oneImported = null;
 		
-		if (this.importedObjects.containsKey(createdOrOrigninal)) {
-			original = createdOrOrigninal;
+		if (this.importedObjects.containsKey(createdOrOriginal)) {
+			//an original was handed
+			original = createdOrOriginal;
+			imported = this.importedObjects.get(createdOrOriginal);
+			this.importedObjects.remove(original);
+			for (EObject imp : imported) {
+				this.originalNodes.remove(imp);
+			}
+		} else if (this.originalNodes.containsKey(createdOrOriginal)) {
+			//one of many imported elements was handed
+			original = this.originalNodes.get(original);
+			oneImported = (EObject)createdOrOriginal;
 			imported = this.importedObjects.get(original);
-		} else if (this.originalNodes.containsKey(createdOrOrigninal)) {
-			imported = (EObject)createdOrOrigninal;
-			original = this.originalNodes.get(imported);
-		} else {
-			return;
+			
+			imported.remove(oneImported);
+			if (imported.isEmpty() ) {
+				this.importedObjects.remove(original);
+			}
+			this.originalNodes.remove(oneImported);			
+		} else if (this.importedObjects.containsValue(createdOrOriginal)) {
+			//a list of imported objects was specified
+			imported = (Set)createdOrOriginal;
+			for (Entry<Object,LinkedHashSet<EObject>>e : this.importedObjects.entrySet()) {
+				if (e.getValue() == createdOrOriginal) {
+					original = e.getKey();
+				}
+			}
+			this.importedObjects.remove(original);
+			for (EObject imp : imported) {
+				this.originalNodes.remove(imp);
+			}
 		}
-		this.originalNodes.remove(imported);
-		this.importedObjects.remove(original);
+	}
+
+	
+	@Override
+	public Collection<EObject> getImportedElements() {
+		return this.originalNodes.keySet();
+	}
+
+	@Override
+	public Collection<Object> getOriginalElements() {
+		return this.importedObjects.keySet();
 	}
 
 
 	@Override
-	public Collection<EObject> getImportedElements() {
-		return this.originalNodes.keySet();
+	public Set<EObject> getImportedElements(Object original) {
+		if (this.importedObjects.containsKey(original)) {
+			return this.importedObjects.get(original);
+		}
+		return null;
+	}
+
+	@Override
+	public EObject getImportedElement(Resource res, Object original) {
+		Set<EObject>set = this.getImportedElements(original);
+		if (set != null) {
+			for (EObject obj : set) {
+				if (obj.eResource() == res) {
+					return obj;
+				}
+			}
+		}
+		return null;
 	}
 
 }
